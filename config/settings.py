f8f42d1f3720df4f88cb4b731e728cf1ec233cc1
HarleyFelipe <harley.cr4ft@gmail.com>
2026-09-24 08:50:13 -0300

    .

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware','django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware','django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -25,7 +26,7 @@ USE_TZ=True
 STATIC_URL='static/'
 STATICFILES_DIRS=[]
 MEDIA_URL='media/'
-MEDIA_ROOT=BASE_DIR/'media'
+MEDIA_ROOT = BASE_DIR/'staticfiles'
 DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
 LOGIN_URL='login'
 LOGIN_REDIRECT_URL='painel'
